c2ba545c994cefa7c9866bcaa63259ac3f3736f6
nikita <nikvladimirov8@gmail.com>
2021-10-27 18:38:01 +0300

    Make a folder tree

---
+++ src/components/mixins/SelectItemMixin.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data () {
+    return {
+      isSelected: false
+    }
+  },
+  methods: {
+    itemClick(event) {
+      event.stopPropagation()
+      this.isSelected = !this.isSelected
+    }
+  }
+}
+</script>
